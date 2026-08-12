f17d3270567dc0f3debaa4512b544c0e3724b52e
Sarjun J <sarjunj2006@gmail.com>
2026-08-12 14:11:42 +0530

    chore: project setup - vite, react, tailwind v4, react-router, axios

---
+++ src/api/axiosClient.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 15000,
+})
+
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+export default axiosClient
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      {/* routes will be added in the next step */}
+    </Routes>
+  )
+}
+
+export default AppRoutes
